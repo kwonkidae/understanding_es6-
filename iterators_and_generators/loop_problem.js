17c9a9b467022743b53070d6dba1373222ed7c89
kkdosk <kkdosk@naver.com>
2019-07-09 07:24:30 +0900

    custom iterator

--- iterators_and_generators/loop_problem.js
+++ iterators_and_generators/loop_problem.js
@@ -7,26 +7,28 @@ for (var i = 0, len = colors.length; i < len; i++) {
 }
 
 (()=>{
-	function createIterator(items) {
-		var i = 0;
-		return {
-			next: function() {
-				var done = (i >= items.length);
-				var value = !done ? items[i++] : undefined;
+	function createIterator(items){
+		return function() {
+			var i = 0;
+			return {
+				next: function() {
+					var done = (i >= items.length);
+					var value = !done ? items[i++] : undefined;
 
-				return {
-					done,
-					value
-				};
-			}
+					return {
+						done,
+						value
+					};
+				}
+			};
 		};
 	}
 
-	var iterator = createIterator([1,2,3]);
-	console.log(iterator.next());
-	console.log(iterator.next());
-	console.log(iterator.next());
-	console.log(iterator.next());
+	var iterator = {};
+	iterator[Symbol.iterator] = createIterator([1,2,3,4,5,6,7,8,9, 10]);
+	console.log(...iterator);
+
+
 	let a = [1,2,3];
 	console.log(...a);
 })();
