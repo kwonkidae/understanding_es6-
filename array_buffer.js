c6abb655a8d794e509116d7ba4e792cc58243b41
kwon <kkdosk@naver.com>
2020-04-21 19:44:47 +0900

    add

---
+++ array_buffer.js
@@ -0,0 +1,9 @@
+let buffer = new ArrayBuffer(2),
+  view = new DataView(buffer);
+
+view.setInt8(0, 5);
+view.setInt8(1, -1);
+
+console.log(view.getInt16(0));
+console.log(view.getInt8(0));
+console.log(view.getInt8(1));
